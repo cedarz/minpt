7741bdcbee55599459fdecac615f6d2bf3140b3d
cedar <dyingthesun@gmail.com>
2024-02-07 11:14:39 +0800

    Create minpt.bat

---
+++ minpt.bat
@@ -0,0 +1,2 @@
+@REM minpt.exe ../CornellBox/CornellBox-Water.obj "" "" result.pfm  100 20 0 1920 1080  0 1 5 0 1 0 30 0 0 0
+minpt.exe ../fireplace_room/fireplace_room.obj "" ../wide.22mm.dat result.pfm 1000 20 0 1920 1080 5.101118 1.083746 -2.756308 4.167568 1.078925 -2.397892 43.001194 3 35 10
